d461e23f5332ed64df34ec0ad019af1b85d07e3d
Lioneriod <123819366+Lioneriod@users.noreply.github.com>
2025-08-26 15:34:05 -0300

    testing flask and jinja!

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-  return render_template("index.html")
+  first_name = "Lio"
+  return render_template("index.html", first_name=first_name)
+
+@app.route("/profile/<name>")
+
+def user(name):
+  return render_template("profile.html", profile_name=name)
